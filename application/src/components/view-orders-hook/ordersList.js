8f903a4f6200c2a65bc155fbdd1fe35d1c3d201b
Robyn Beck <deft.or.die@gmail.com>
2021-10-19 18:32:08 -0700

    bug(ordersList): fixed time format

--- application/src/components/view-orders-hook/ordersList.js
+++ application/src/components/view-orders-hook/ordersList.js
@@ -1,32 +1,55 @@
 import React from 'react';
 
-const OrdersList = (props) => {
-    const { orders } = props;
-    if (!props || !props.orders || !props.orders.length) return (
-        <div className="empty-orders">
-            <h2>There are no orders to display</h2>
-        </div>
-    );
+const OrdersList = props => {
+  const { orders } = props
+  if (!props || !props.orders || !props.orders.length)
+    return (
+      <div className='empty-orders'>
+        <h2>There are no orders to display</h2>
+      </div>
+    )
 
-    return orders.map(order => {
-        const createdDate = new Date(order.createdAt);
-        return (
-            <div className="row view-order-container" key={order._id}>
-                <div className="col-md-4 view-order-left-col p-3">
-                    <h2>{order.order_item}</h2>
-                    <p>Ordered by: {order.ordered_by || ''}</p>
-                </div>
-                <div className="col-md-4 d-flex view-order-middle-col">
-                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
-                    <p>Quantity: {order.quantity}</p>
-                </div>
-                <div className="col-md-4 view-order-right-col">
-                    <button className="btn btn-success">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
-                </div>
-            </div>
-        );
-    });
+  return orders.map(order => {
+    const currentLanguage = window.navigator.language
+    const formatOptions = {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    }
+
+    const dateTimeFormat = new Intl.DateTimeFormat(currentLanguage, formatOptions)
+    const date = new Date()
+    const createdDate = dateTimeFormat.format(date)
+    return (
+      <div className='row view-order-container' key={order._id}>
+        <div className='col-md-4 view-order-left-col p-3'>
+          <h2>{order.order_item}</h2>
+          <p>Ordered by: {order.ordered_by || ''}</p>
+        </div>
+        <div className='col-md-4 d-flex view-order-middle-col'>
+          <p>Order placed at {createdDate}</p>
+          <p>Quantity: {order.quantity}</p>
+        </div>
+        <div className='col-md-4 view-order-right-col'>
+          <button
+            className='btn btn-success'
+            onClick={event => {
+              event.preventDefault()
+
+              console.log(
+                { Window_Object: window },
+                { Language_Info: window.navigator.language },
+                'hello'
+              )
+            }}
+          >
+            Edit
+          </button>
+          <button className='btn btn-danger'>Delete</button>
+        </div>
+      </div>
+    )
+  })
 }
 
-export default OrdersList;
+export default OrdersList
